c0f173c80a81b4c05ce7a70126f5028f925cd20a
jayrsawal <jayrsawal@gmail.com>
2018-01-30 13:40:33 -0500

    Change server port to support nginx

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express   = require('express');
 const app       = express();
-const port      = 3000;
+const port      = 8081;
 
 const AWS = require('aws-sdk');
 AWS.config.region = process.env.REGION
